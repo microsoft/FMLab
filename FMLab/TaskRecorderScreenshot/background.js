6731396678e5151f6dc65cbaed51f943e3df7b5d
Capgemini-DOliver <117965025+Capgemini-DOliver@users.noreply.github.com>
2022-11-11 15:34:30 +1100

    Task recorder screenshot for chrome manifest v3
    Updated task recorder screenshot extension to comply with chrome manifest v3.

--- FMLab/TaskRecorderScreenshot/background.js
+++ FMLab/TaskRecorderScreenshot/background.js
@@ -1,8 +1,9 @@
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.name == 'screenshot') {
-        chrome.tabs.captureVisibleTab(null, null, function(dataUrl) {			
-            sendResponse({ screenshotUrl: dataUrl });
-        });
-    }
-    return true;
-});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.name == 'screenshot') {
+            chrome.tabs.captureVisibleTab(null, null, function(dataUrl) {			
+                sendResponse({ screenshotUrl: dataUrl });
+            });
+        }
+        return true;
+    });
